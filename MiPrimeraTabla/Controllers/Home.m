177a1f8fc1bea3ef76f3bbb511a54247d8bf2613
Jose Trinidad Fajardo Flores <trinof@Joses-Mac.local>
2017-09-26 18:33:54 -0700

    Added new pics Configured table delegate methods Created dynamic list

--- MiPrimeraTabla/Controllers/Home.m
+++ MiPrimeraTabla/Controllers/Home.m
@@ -7,15 +7,20 @@
 //
 
 #import "Home.h"
+#import "CellMainTable.h"
 
 @interface Home ()
-
+@property NSMutableArray *userNames;
+@property NSMutableArray *userAges;
+@property NSMutableArray *userImages;
 @end
 
 @implementation Home
-
-- (void)viewDidLoad {
+/**********************************************************************************************/
+#pragma mark - Initialization methods
+/**********************************************************************************************/- (void)viewDidLoad {
     [super viewDidLoad];
+    [self initController];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +28,58 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+//-------------------------------------------------------------------------------
+- (void)initController {
+    self.userNames  = [[NSMutableArray alloc] initWithObjects: @"Tyrion Lannister", @"Daenerys Targaryen", @"Jon Snow", @"Arya Stark", @"Cersei Lannister", nil];
+    
+    self.userAges  = [[NSMutableArray alloc] initWithObjects: @"38 años", @"22 años", @"25 años", @"16 años", @"42 años", nil];
+    
+    self.userImages = [[NSMutableArray alloc] initWithObjects: @"tyrion.jpg", @"daenerys.jpeg", @"jon.jpg", @"arya.jpg", @"cersei.jpg", nil];
+}
+
+/**********************************************************************************************/
+#pragma mark - Table source and delegate methods
+/**********************************************************************************************/
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+//-------------------------------------------------------------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.userNames.count;
+}
+//-------------------------------------------------------------------------------
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 64;
+}
+//-------------------------------------------------------------------------------
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Initialize cells
+    CellMainTable *cell = (CellMainTable *)[tableView dequeueReusableCellWithIdentifier:@"CellMainTable"];
+    
+    if (cell == nil) {
+        [tableView registerNib:[UINib nibWithNibName:@"CellMainTable" bundle:nil] forCellReuseIdentifier:@"CellMainTable"];
+        cell = [tableView dequeueReusableCellWithIdentifier:@"CellMainTable"];
+    }
+    //Fill cell with info from arrays
+    cell.lblName.text       = self.userNames[indexPath.row];
+    cell.lblAge.text        = self.userAges[indexPath.row];
+    cell.imgUser.image      = [UIImage imageNamed:self.userImages[indexPath.row]];
+    
+    return cell;
+}
+//-------------------------------------------------------------------------------
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Pending
+}
+/**********************************************************************************************/
+#pragma mark - Action methods
+/**********************************************************************************************/
+- (IBAction)btnAddPressed:(id)sender {
+    [self.userNames addObject:@"Walter"];
+    [self.userAges addObject:@"37 años"];
+    [self.userImages addObject:@"jon.jpg"];
+    [self.tblMain reloadData];
 }
 
 
